78806795411043d4deffef017c770ff6b8a34a77
wiliancalora <wiliancalora@evoluaeducacao.com.br>
2020-04-19 11:38:16 -0300

    finalização desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((antcu, cur) => antcu + cur.value, 0);
+
+    const outcome = this.transactions
+      .filter(trasaction => trasaction.type === 'outcome')
+      .reduce((antcur, cur) => antcur + cur.value, 0);
+
+    if (income < outcome) {
+      throw Error('Insufficient funds');
+    }
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
